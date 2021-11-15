717704aa8f6984bd2b431a5e8737bd517b099dd2
Michael Guevara Moncada <graficartecr@gmail.com>
2021-11-15 12:37:07 -0600

    Adding Modules SEARCH and SHOW CONTACTS

--- Klients.py
+++ Klients.py
@@ -48,8 +48,38 @@ def add_client(clients, data_base):
     if clients.get(name):
         print(f'Client: {name} already exist')
     else:
-        name = input("Full name: ")
+        phone_number = input("Phone number: ")
+        email = input("Email: ")
+        id_ = input("Identification: ")
+        product_type = input("Product type: ")
+        price = input("Price: ")
 
+        clients[os.name] = (phone_number, email, id_, product_type, price, DATE) 
+        file = open('data_base', 'a')
+        file.write(f'{name}, {phone_number}, {email}, {id_}, {product_type}, {price}, {DATE}\n')
+        print('\n**** Contacto agregado con exito ****\n')
 
+#SHOW CONTACTS METHOD
+def show_contacts():
+    os.system('cls')
+    print('     Adding New Client')
+    print('=' * 30)
+
+    if len(data_base) > 0:
+        for contact, data in data_base.items():
+            print(f'Name: {contact}')
+            print(f'Phone: {data[0]}')
+            print(f'Email: {data[1]}')
+            print(f'ID: {data[2]}')
+            print(f'Product: {data[3]}')
+            print(f'Price: {data[4]}')
+            print(f'Date: {data[5]}')
+            print('=' * 40)
+    else :
+        print('There are no contact registered.')
+
+#SEARCH CONTACT METHOD
+def search_contac():
+           
 # file = open("db/" + name + "_" + id_ + ".txt", "w")
 #git push -u origin main
